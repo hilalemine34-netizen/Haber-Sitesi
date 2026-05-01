e36b161ff2d745e4b349800e529551ada926dec0
Hilal İlhan <hilalemine34@gmail.com>
2026-05-01 20:24:23 +0300

    db error handle eklendi

--- repositories/base_repository.py
+++ repositories/base_repository.py
@@ -3,13 +3,17 @@ import mysql.connector
 class BaseRepository:
 
     def get_connection(self):
-        return mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="K334z91h36.",
-            database="news_tracking_db",
-            autocommit=True
-        )
+        try:
+            return mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="...",
+                database="news_tracking_db",
+                autocommit=True
+            )
+        except Exception as e:
+            print("DB bağlantısı kurulamadı:", e)
+            return None
 
     def get_cursor(self, dictionary=False):
         conn = self.get_connection()
